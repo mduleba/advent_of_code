2967f4c24050084c250ef6240760c7414c01b8c4
michal <michal@solvbot.pl>
2022-12-09 09:33:51 +0100

    wipppp

--- 2022/9/bridge.py
+++ 2022/9/bridge.py
@@ -3,7 +3,8 @@ from __future__ import annotations
 import copy
 import math
 from dataclasses import dataclass, field
-from typing import List, Tuple, Literal
+from typing import List, Tuple, Literal, Optional
+import matplotlib.pyplot as plt
 
 
 @dataclass
@@ -29,18 +30,23 @@ class Coords:
     def __eq__(self, other: Coords):
         return self.x == other.x and self.y == other.y
 
+    def get_simple(self):
+        return self.x, self.y
+
     def difference(self, coords: Coords):
         return math.fabs(self.x - coords.x) + math.fabs(self.y - coords.y)
 
-    def axis_with_bigger_difference(self, coords: Coords):
-        x_diff = math.fabs(self.x - coords.x)
-        y_diff = math.fabs(self.y - coords.y)
-        if x_diff > y_diff:
-            return 'x'
-        elif y_diff > x_diff:
-            return 'y'
-        else:
+    def find_point_between_coords(self, coords: Coords) -> Optional[Coords]:
+        if self == coords or self.difference(coords) <= 2:
             return None
+        else:
+            x_diff = math.fabs(self.x - coords.x)
+            y_diff = math.fabs(self.y - coords.y)
+
+            if x_diff > y_diff:
+                return Coords(int(coords.x - (x_diff // 2)), coords.y)
+            elif y_diff > x_diff:
+                return Coords(coords.x, int(coords.y - (y_diff // 2)))
 
 
 @dataclass
@@ -51,6 +57,7 @@ class Move:
 class Entity:
     def __init__(self, coords: Coords):
         self.coords: Coords = coords
+        self.logs: List[Tuple[int, int]] = []
 
     def move(self, movement: Move):
         self.coords += movement
@@ -64,25 +71,32 @@ class Entity:
 
 class Tail(Entity):
     def update(self, head: Head):
-        if self.coords != head.coords:
-
+        if self.coords.difference(head.coords) > 1:
             if self.same_x(head):
                 if head.coords.y > self.coords.y:
                     self.move(Move('U'))
                 else:
                     self.move(Move('D'))
-
             elif self.same_y(head):
                 if head.coords.x > self.coords.x:
                     self.move(Move('R'))
                 else:
                     self.move(Move('L'))
 
-            elif self.coords.difference(head.coords) > 2:
-                pass
+            elif point_between := self.coords.find_point_between_coords(head.coords):
+                self.coords = point_between
 
-            else:
-                pass
+            self.logs.append(self.coords.get_simple())
+
+    def show_plot(self):
+        xx = []
+        yy = []
+        for x, y in self.logs:
+            xx.append(x)
+            yy.append(y)
+
+        plt.scatter(xx, yy, s=100)
+        plt.show()
 
 
 class Head(Entity):
@@ -111,10 +125,32 @@ def load_actions(file_name):
     return actions
 
 
+class Test:
+
+    def __init__(self, file_name):
+        self.actions = load_actions(file_name)
+        self.tail = Tail(coords=Coords(1, 1))
+        self.head = Head(coords=Coords(1, 1), tail=self.tail)
+
+    def run(self):
+        assert len(self.actions) == 24
+        for movement in self.actions:
+            self.head.move(movement)
+        assert self.head.coords == Coords(3, 3)
+        assert self.tail.coords == Coords(2, 3)
+        assert len(set(self.tail.logs)) == 13
+
+
+test_case = Test('test_input.txt')
+test_case.run()
+
+
 actions = load_actions('input.txt')
 
-tail = Tail(coords=Coords(0, 0))
-head = Head(coords=Coords(1, 0), tail=tail)
+tail = Tail(coords=Coords(1, 1))
+head = Head(coords=Coords(1, 1), tail=tail)
 
 for movement in actions:
     head.move(movement)
+
+print(len(set(tail.logs)))
